5e877c35a31abfad29685a23890473d39522ce77
KDThaGoatt <kyandupuis@gmail.com>
2026-05-05 11:28:58 -0700

    A

--- assignment.py
+++ assignment.py
@@ -32,7 +32,7 @@ query = "create table if not exists vet (id integer primary key autoincrement, p
 
 cursor.execute(query)
 
-def inputs():
+def insert():
     pet_name = input("Enter your pet's name: ")
     pet_species = input("Enter your pet's species (cat, bird, dog, etc): ")
     pet_breed = input("Enter your pet's breed (persian, beagle, canary, etc): ")
@@ -41,11 +41,47 @@ def inputs():
     owner_email = input("Enter your email: ")
     owner_balance = input("Enter the amount you owe: ")
     date_first_visit = input("Enter the date of your first visit: ")
+    query = "insert into vet (petname, petspecies, petbreed, ownername, ownerphonenumber, owneremail, ownerbalance, datefirstvisit) values (?, ?, ?, ?, ?, ?, ?, ?)"
+    values = (pet_name, pet_species, pet_breed, owner_name, owner_phone_number, owner_email, owner_balance, date_first_visit)
+    cursor.execute(query, values)
+
+def idret():
+    id_input = input("Which id do you want to retrieve for?: ")
+    query = f"select * from vet where id={id_input}"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    print(f"{'ID':>3} {'Pet Name':10} {'Pet Species':10} {'Pet Breed':20} {'Owner Name':20} {'Owner Phone Number':20} {'Owner Email':25} {'Owner Balance':5} {'Date First Visit':15}")
+    for i in result:
+        print(f"{i[0]:3} {i[1]:10} {i[2]:10} {i[3]:<20} {i[4]:<20} {i[5]:<20} {i[6]:<25} {i[7]:<5} {i[8]:<15}")
+
+def emailret():
+    email_input = input("Which email do you want to retrieve for?: ")
+    query = f"select * from vet where owneremail={email_input}"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    print(f"{'ID':>3} {'Pet Name':10} {'Pet Species':10} {'Pet Breed':20} {'Owner Name':20} {'Owner Phone Number':20} {'Owner Email':25} {'Owner Balance':5} {'Date First Visit':15}")
+    for i in result:
+        print(f"{i[0]:3} {i[1]:10} {i[2]:10} {i[3]:<20} {i[4]:<20} {i[5]:<20} {i[6]:<25} {i[7]:<5} {i[8]:<15}")
+
+def pnret():
+    phone_number_input = input("Which phone number do you want to retrieve for?: ")
+    query = f"select * from vet where ownerphonenumber={phone_number_input}"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    print(f"{'ID':>3} {'Pet Name':10} {'Pet Species':10} {'Pet Breed':20} {'Owner Name':20} {'Owner Phone Number':20} {'Owner Email':25} {'Owner Balance':5} {'Date First Visit':15}")
+    for i in result:
+        print(f"{i[0]:3} {i[1]:10} {i[2]:10} {i[3]:<20} {i[4]:<20} {i[5]:<20} {i[6]:<25} {i[7]:<5} {i[8]:<15}")
 
 def start():
     print("Would you like to \nA: Insert a new record into the database \nB: Retrieve a record by id \nC: Retrieve a record by email \nD: Retrieve a record by phone number")
     question = input("Type A, B, C, or D: ")
     if question == 'A' or question == 'a':
-        inputs()
+        insert()
     elif question == 'B' or question == 'b':
-        
+        idret()
+    elif question == 'C' or question == 'c':
+        emailret()
+    elif question == 'D' or question == 'd':
+        pnret()
+
+start()
